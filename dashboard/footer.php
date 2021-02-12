eadc25eb1b6cfecdd5631e2e111b5f904f13e32e
Muhammed Ahmed Salim <65686246+Ahmed-Salim@users.noreply.github.com>
2021-02-12 16:40:50 +0500

    add footer

--- dashboard/footer.php
+++ dashboard/footer.php
@@ -1,3 +1,10 @@
+<footer class="page-footer">
+    <div class="footer-copyright">
+        <div class="container">
+            © <?php echo date("Y"); ?> <a class="white-text" href="<?php echo urlPrefix(); ?>">Muzlock</a>
+        </div>
+    </div>
+</footer>
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
